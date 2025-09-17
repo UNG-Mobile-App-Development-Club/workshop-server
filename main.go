08ecaaedaec8e622074fc255f872eca8951c0774
Devin <devin24beck@gmail.com>
2025-09-17 10:27:17 -0400

    Added todo post handler

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ func main() {
     router.Use(middleware.Logger)
 
     router.Get("/todos", getTodos)
+    router.Post("/todos", postTodo)
 
     http.ListenAndServe(":8080", router)
 }
@@ -40,4 +41,20 @@ func getTodos(w http.ResponseWriter, r *http.Request) {
 
     w.Header().Set("Content-Type", "application/json")
     w.Write(raw)
+}
+
+func postTodo(w http.ResponseWriter, r *http.Request) {
+    var todo Todo
+    if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
+	http.Error(w, err.Error(), http.StatusBadRequest)
+	return
+    }
+
+    todo.Id = len(todos) + 1
+    todo.CreatedAt = time.Now()
+    todos = append(todos, todo)
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(todo)
 }
